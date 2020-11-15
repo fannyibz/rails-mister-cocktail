478d969362b0d28b5eef2c6205012b334ad2bdcf
fannyibanez <fannyibanez89@gmail.com>
2020-11-15 13:59:13 +0100

    heroku connection

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -6,5 +6,7 @@ class Dose < ApplicationRecord
   validates :ingredient_id, presence: true
   validates :cocktail_id, presence: true
   validates_uniqueness_of :cocktail_id, scope: :ingredient_id
+  validates :ingredient, uniqueness: { scope: :cocktail,
+    message: "should create only one dose per cocktail and ingredient" }
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,8 @@ Rails.application.routes.draw do
   # get 'cocktails/create'
   # get 'cocktails/show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :cocktails, only: [:index, :new, :create, :show] do
+  root to: "cocktails#index"
+  resources :cocktails, only: [:new, :create, :show] do
     resources :doses, only: [:new, :create]
   end
   resources :doses, only: [:destroy]
