c60936ec699a6659cdb8449c63e003c55b6a3063
fannyibanez <fannyibanez89@gmail.com>
2020-11-15 15:31:51 +0100

    Add dotenv - Protect my secret data in .env file

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,18 +1,22 @@
 class DosesController < ApplicationController
-
   def new
     @cocktail = Cocktail.find(params[:cocktail_id])
     @dose = Dose.new(cocktail_id: @cocktail.id)
   end
 
   def create
-    @dose = Dose.new
+    @dose = Dose.new(dose_params)
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose.cocktail = @cocktail
 
-    redirect_to restaurant_path(@restaurant)
+    if @dose.save
+      redirect_to cocktail_path(@dose.cocktail)
+    else
+      render :new
+    end
   end
 
-  def destroy
-  end
+  def destroy;end
 
   private
 
--- app/models/dose.rb
+++ app/models/dose.rb
@@ -1,11 +1,15 @@
 class Dose < ApplicationRecord
   belongs_to :cocktail
   belongs_to :ingredient
+  validates_associated :cocktail, :ingredient
 
   validates :description, presence: true
-  validates :ingredient_id, presence: true
-  validates :cocktail_id, presence: true
-  validates_uniqueness_of :cocktail_id, scope: :ingredient_id
+
+  # validates :ingredient_id, presence: true
+  # validates :cocktail_id, presence: true
+
+
+  # validates_uniqueness_of :cocktail_id, scope: :ingredient_id
   validates :ingredient, uniqueness: { scope: :cocktail,
     message: "should create only one dose per cocktail and ingredient" }
 
