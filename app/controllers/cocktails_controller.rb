7ba629c602cbc6b25093c207cf0fd49650116c68
fannyibanez <fannyibanez89@gmail.com>
2020-11-15 15:58:48 +0100

    cloundinary installation

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -12,9 +12,12 @@ class CocktailsController < ApplicationController
   def create
     @cocktail = Cocktail.new(cocktail_params)
     @ingredient = Ingredient.all
-    @cocktail.save
 
-    redirect_to cocktail_path(@cocktail)
+    if @cocktail.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
   end
 
   def show
@@ -27,7 +30,7 @@ class CocktailsController < ApplicationController
   end
 
   def cocktail_params
-    params.require(:cocktail).permit(:name, :ingredient, :dose)
+    params.require(:cocktail).permit(:name, :ingredient, :dose, :photo)
   end
 
 end
